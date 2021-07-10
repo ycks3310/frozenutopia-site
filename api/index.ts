6a2ed8bb0d15dc05d17d57e991f15d1760f567c9
Kei Sato <kei.sato@frozenutopia.net>
2021-07-10 11:37:17 +0900

    入力したテキストの読み仮名を返す

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
+const kuromoji = require('kuromoji')
 
 app.use(express.json({ extended: true, limit: '100mb' }))
 app.use(express.urlencoded({ extended: true, limit: '100mb' }))
 
-app.get('/test', (req: any, res: any) => {
-  return res.status(200).json(['OK'])
+app.get('/shiritori', (req: any, res: any) => {
+  const text = req.query.text
+  kuromoji.builder({ dicPath: './node_modules/kuromoji/dict'}).build((err: any, tokenizer: any) => {
+    const path = tokenizer.tokenize(text)
+    let yomigana: string = ''
+    path.forEach((element: any) => {
+      console.log(element.reading)
+      yomigana = yomigana + element.reading
+    })
+    return res.status(200).json({
+      input: text,
+      output: yomigana
+    })
+  })
+
 })
 
 module.exports = {
