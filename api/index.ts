27b0353814a67ac14168ae86ff68c3abfb3fbc00
Kei Sato <kei.sato@frozenutopia.net>
2021-07-10 16:44:24 +0900

    複数単語から作られている言葉はルール違反としてエラーを返す

--- api/index.ts
+++ api/index.ts
@@ -7,19 +7,16 @@ app.use(express.urlencoded({ extended: true, limit: '100mb' }))
 
 const KUROMOJI_DIC_PATH = './node_modules/kuromoji/dict'
 
-function getFurigana(text: string): Promise<string> {
+function getFurigana(text: string): Promise<string|null> {
   return new Promise((resolve, reject) => {
     kuromoji.builder({ dicPath: KUROMOJI_DIC_PATH}).build((err: any, tokenizer: any) => {
-      if(err) {
-        reject(err)
-      }
       const path = tokenizer.tokenize(text)
-      let furigana: string = ''
-      path.forEach((element: any) => {
-        // console.log(element.reading)
-        furigana = furigana + element.reading
-      })
-      resolve(furigana)
+      // 複数個の単語を繋げて作られている単語はルール違反とする
+      //（アメリカ合衆国大統領など）
+      if(path.length != 1) {
+        resolve(null)
+      }
+      resolve(path[0].reading)
     })
   })
 }
@@ -32,7 +29,7 @@ app.get('/shiritori', async (req: any, res: any) => {
 
   // ひらがな、カタカナ、漢字の判定
   const japanese_regex = /^[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]+$/
-  console.log(input.match(japanese_regex))
+  // console.log(input.match(japanese_regex))
   if (input.match(japanese_regex) === null) {
     return res.status(400).json({ code: -1, error: 'text expects only Japanese'})
   }
@@ -41,10 +38,10 @@ app.get('/shiritori', async (req: any, res: any) => {
     .then((result) => {
       return result
     })
-    .catch(e => {
-      console.log(e)
-      return res.status(500).json({code: -1, error: e})
-    })
+
+  if(furigana === null) {
+    return res.status(400).json({code: -1, error: 'Number of words is larger than 1'})
+  }
 
   return res.status(200).json({
     code: 0,
