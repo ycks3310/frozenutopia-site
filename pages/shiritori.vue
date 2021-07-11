c522b44ccf74f4f301d31dd6d1bfc875bcf5badd
Kei Sato <kei.sato@frozenutopia.net>
2021-07-11 09:33:53 +0900

    しりとりページがわだけ

---
+++ pages/shiritori.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app light>
+    <v-container>
+      <h1> しりとり </h1>
+    </v-container>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Shiritori extends Vue {
+  hoge = true
+}
+</script>
